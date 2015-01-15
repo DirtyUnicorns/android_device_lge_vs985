82e9ea4291ff1d008b114158fbcc8981571ad30d
Daniel Saporita <dajasa22793@gmail.com>
2015-01-15 17:13:40 -0500

    Update and rename cm.mk to du.mk
    initial DU support

---
+++ du.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/lge/vs985/full_vs985.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="du_vs985" \
+    PRODUCT_NAME="g3_vzw_us" \
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon

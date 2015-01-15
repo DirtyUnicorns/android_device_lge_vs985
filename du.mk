a22a7187d7202f18c91ea41491663d93845b7ebf
Daniel Saporita <dajasa22793@gmail.com>
2015-01-15 17:45:13 -0500

    Update du.mk

--- du.mk
+++ du.mk
@@ -5,6 +5,8 @@ $(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 TARGET_SCREEN_WIDTH := 1440
 TARGET_SCREEN_HEIGHT := 2560
 
+PRODUCT_NAME := du_vs985
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="du_vs985" \
     PRODUCT_NAME="g3_vzw_us" \
